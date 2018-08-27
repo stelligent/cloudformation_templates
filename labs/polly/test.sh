a4586a41d36d613e95a3923fa412fee10afd0f68
Paul Duvall <pduvall@gmail.com>
2018-08-27 15:44:28 +0000

    get aws version

--- labs/polly/test.sh
+++ labs/polly/test.sh
@@ -12,3 +12,5 @@ echo $testvar
 # aws polly synthesize-speech --output-format mp3 --voice-id Brian --text "$testvar" polly3.mp3
 aws polly start-speech-synthesis-task --output-format mp3 --output-s3-bucket-name delete-pmd-guardduty --text "$testvar" --voice-id Brian 
 # aws s3 cp polly4.mp3 s3://delete-pmd-guardduty/polly4.mp3
+
+# aws polly start-speech-synthesis-task --output-format mp3 --voice-id Joanna --output-s3-bucket-name delete-pmd-guardduty --text ./labs/polly/blog.txt hello.mp3
